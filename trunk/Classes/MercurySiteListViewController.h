6cb879f5b35f11d822fbed4677c1d3aada383743
nacci520 <nacci520@750de19f-6b88-372b-2a80-9d854a6c414a>
2011-04-01 14:50:56 +0000

    maintaince

--- trunk/Classes/MercurySiteListViewController.h
+++ trunk/Classes/MercurySiteListViewController.h
@@ -12,13 +12,31 @@
  * Customize site list root view controller here.
  * Push new view by appDelegate.mercuryMainboardViewcontroller.sitesListNavigationController.view.
  */
+#define NSITES 6
+#define PARANUM 6
+
+#define DEFAULT_TABBAR_HEIGHT 49
+#define DEFAULT_NAVBAR_HEIGHT 44
+
+#define SCREEN_WIDTH screenRect.size.width
+#define SCREEN_HEIGHT screenRect.size.height
+#define TABLEVIEW_CONTENT_HEIGHT NSITES * 85
+
+
+
 @interface MercurySiteListViewController : 
 UIViewController<UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate> {
+    // Parameter indicator
     UIPageControl *siteIndicator;
+    // ParamaterScroll is a subview of siteScroll.
     UIScrollView *siteScroll;
     UIScrollView *paramaterScroll;
+    // The table holds all site names.
     UITableView *generalInfoTable;
+    // The table holds paramater plots.
     UITableView *corePlotInfoTable;
+    // The big view hold all plot tables
+    UIView *plotHolders;
 }
 
 
