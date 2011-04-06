6ff388fea9eb6f780c07886cb57b5b97b96033b2
nacci520 <nacci520@750de19f-6b88-372b-2a80-9d854a6c414a>
2011-04-06 03:08:04 +0000

    Add implementation of main site list UI

--- trunk/Classes/MercurySiteListViewController.h
+++ trunk/Classes/MercurySiteListViewController.h
@@ -2,24 +2,37 @@
 //  MercurySiteListViewController.h
 //  Mercury
 //
-//  Created by Jeffrey on 11-3-25.
+//  Created by puretears on 11-3-25.
 //  Copyright 2011年 __MyCompanyName__. All rights reserved.
 //
-
+#include <iostream>
+#include <list>
 #import <UIKit/UIKit.h>
 
 /* 
  * Customize site list root view controller here.
  * Push new view by appDelegate.mercuryMainboardViewcontroller.sitesListNavigationController.view.
  */
-#define NSITES 6
-#define PARANUM 6
+#define NSITES 20
+
+#define COREPLOT_TAG_BASE   0
+#define NEVEL_ENABLE_TABLE  0
+#define AVAIL_TABLE         1
+#define PV_TABLE            2
+#define THREAT_TABLE        3
+#define LATENCY_TABLE       4
+#define SECURITY_LOGS_TABLE 5
+#define ALERT_LOGS_TABLE    6
+
+#define PARANUM 7
+
+#define PAGEINDICATOR_HEIGHT 15
 
 #define DEFAULT_TABBAR_HEIGHT 49
 #define DEFAULT_NAVBAR_HEIGHT 44
 
-#define SCREEN_WIDTH screenRect.size.width
-#define SCREEN_HEIGHT screenRect.size.height
+#define SCREEN_WIDTH [[UIScreen mainScreen] bounds].size.width
+#define SCREEN_HEIGHT [[UIScreen mainScreen] bounds].size.height
 #define TABLEVIEW_CONTENT_HEIGHT NSITES * 85
 
 
@@ -27,7 +40,7 @@
 @interface MercurySiteListViewController : 
 UIViewController<UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate> {
     // Parameter indicator
-    UIPageControl *siteIndicator;
+    UIPageControl *paraIndicator;
     // ParamaterScroll is a subview of siteScroll.
     UIScrollView *siteScroll;
     UIScrollView *paramaterScroll;
@@ -37,7 +50,27 @@ UIViewController<UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegat
     UITableView *corePlotInfoTable;
     // The big view hold all plot tables
     UIView *plotHolders;
+    // The plot table list.
+    std::list<UITableView *> *corePlotList;
+    
+@private
+    /* 
+     * Save this position to judge whether user scroll the 
+     * paramater table from left or right.
+     */
+    CGPoint originScrollOffset;
+    // Save this index to update the title on the navigation bar.
+    int currentTableIndex;
+    /* 
+     * NOTE: we should load the UI from UI.plist.
+     * DEMO only!
+     */
+    NSArray *paraTitle;
+    NSArray *plotColor;
 }
 
+@property (nonatomic, retain) UIView *plotHolders;
+@property (nonatomic, retain) UIScrollView *paramaterScroll;
 
+- (void)tableTurn:(UIPageControl *)pageControl;
 @end
---
+++ trunk/Classes/test.h
@@ -0,0 +1,7 @@
+//
+//  test.h
+//  Mercury
+//
+//  Created by puretears on 4/1/11.
+//  Copyright 2011 Rising. All rights reserved.
+//
